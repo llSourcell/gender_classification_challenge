e4331f4fb273d487e44ff15d24e040a31d456888
Rohan Verma <rohanverma2004@gmail.com>
2016-10-08 17:20:10 +0530

    fix: PEP8 issues and Python3 Print

--- demo.py
+++ demo.py
@@ -2,23 +2,25 @@ from sklearn import tree
 
 clf = tree.DecisionTreeClassifier()
 
-## CHALLENGE - create 3 more classifiers...
-#1
-#2
-#3
+# CHALLENGE - create 3 more classifiers...
+# 1
+# 2
+# 3
 
-#[height, weight, shoe_size]
-X = [[181, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37], [166, 65, 40], [190, 90, 47], [175, 64, 39],
+# [height, weight, shoe_size]
+X = [[181, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37], [166, 65, 40],
+     [190, 90, 47], [175, 64, 39],
      [177, 70, 40], [159, 55, 37], [171, 75, 42], [181, 85, 43]]
 
-Y = ['male', 'male', 'female', 'female', 'male', 'male', 'female', 'female', 'female', 'male', 'male']
+Y = ['male', 'male', 'female', 'female', 'male', 'male', 'female', 'female',
+     'female', 'male', 'male']
 
 
-#CHALLENGE - ...and train them on our data
+# CHALLENGE - ...and train them on our data
 clf = clf.fit(X, Y)
 
 prediction = clf.predict([[190, 70, 43]])
 
-#CHALLENGE compare their reusults and print the best one!
+# CHALLENGE compare their reusults and print the best one!
 
-print prediction
+print(prediction)
